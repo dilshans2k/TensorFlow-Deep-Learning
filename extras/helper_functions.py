2939754f5a7b52cd8bb470a60bccfb811d96fdc3
Dilshan Sandhu <67574348+dilshans2k@users.noreply.github.com>
2021-07-18 16:25:23 +0530

    Added walkthrough-dir

--- extras/helper_functions.py
+++ extras/helper_functions.py
@@ -20,7 +20,25 @@ def unzip_data(filename):
   zip_ref.extract_all()
   zip_ref
 
+  
+def walk_through_dir(dir_path):
+  """
+  Walks through dir_path returning its contents.
+  Args:
+    dir_path (str): target directory
+  
+  Returns:
+    A print out of:
+      number of subdiretories in dir_path
+      number of images (files) in each subdirectory
+      name of each subdirectory
+  """
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
 
+    
+    
+    
 #Pre-Processing
 def view_random_images(dirpath, classname):
   plt.figure(figsize = (10,7))
